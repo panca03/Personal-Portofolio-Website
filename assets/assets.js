4e292ff71499bd9ab07912ef8290d444f211c3db
Panca03 <pancawigu080@gmail.com>
2025-09-19 00:22:42 +0700

    update cv and desctiption experience

--- assets/assets.js
+++ assets/assets.js
@@ -226,10 +226,12 @@ export const experience = [
 ◦ Managed and updated the official Computer Vision Lab website (https://comvis.mystrikingly.com) to ensure that the information, content, and resources were accessible and up to date.`,
   },
   {
-    year: '2024 - 2021',
+    year: 'Sept 2025',
     role: 'Website & Apps Development Learnig Path',
     company: 'E-Learning - MySkill',
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+    description: `◦ Completed fundamental web development modules covering front-end, back-end, API, cloud, and responsive design.
+◦ Gained practical skills in HTML, CSS, and JavaScript, including API integration and IDE usage.
+◦ Acquired insights into career pathways and best practices in professional web development.`,
     certificateUrl:
       'https://storage.googleapis.com/myskill-v2-certificates/topic-IXppB88r5Rj19e8Pm65p/bF0sUbxOkEgfC0NUhOAJlzCLsGC2-qwg22Frlp1gHMMkSeKhy.pdf',
   },
@@ -237,20 +239,20 @@ export const experience = [
 
 const Github = () => (
   <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="lucide lucide-github"
-    >
-      <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
-      <path d="M9 18c-4.51 2-5-2-7-2" />
-    </svg>
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="lucide lucide-github"
+  >
+    <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+    <path d="M9 18c-4.51 2-5-2-7-2" />
+  </svg>
 );
 
 const ExternalLink = () => (
